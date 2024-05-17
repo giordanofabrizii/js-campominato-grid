7fc41a7ade9af12a7f0297f472c00509b9bd2b03
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-17 14:31:28 +0200

    added selection of the difficulty

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const campoContainerEl = document.getElementById("campo-container");
 
-let numberOfCell = 100;
-
 const playButton = document.getElementById("start-game");
 playButton.addEventListener('click', function(){
+    
+    // definisco difficolta
+    let difficultyChoiceEl = document.getElementById("difficulty-choice")
+    let difficulty = difficultyChoiceEl.value;
+
+    //  definisco il numero di celle
+    let numberOfCell;
+    if (difficulty === "easy") {
+        numberOfCell = 49;
+    } else if(difficulty === "normal") {
+        numberOfCell = 81;
+    } else {
+        numberOfCell = 100;
+    }
+
+
     // prima svuoto il container
     campoContainerEl.innerHTML = ''
 
@@ -11,7 +25,7 @@ playButton.addEventListener('click', function(){
     for (let i = 1; i < numberOfCell + 1; i++) {
         let article = document.createElement("article");
         article.innerHTML = `<p>${i}</p>`
-        article.classList.toggle("cella");
+        article.classList.add("cella" , `${difficulty}`);
         campoContainerEl.appendChild(article);
 
         article.addEventListener('click', function(){
